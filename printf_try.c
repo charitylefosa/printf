6649b2687950959822fc48c6094017d69540bb13
Mcminn <mcminntheonly@gmail.com>
2023-04-25 17:31:55 -0700

    update

--- printf_try.c
+++ printf_try.c
@@ -37,6 +37,82 @@ int print_string(const char *s)
 	}
 	return (count);
 }
+
+int print_number(int n)
+{
+	int count = 0;
+
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+		count++;
+	}
+	if (n / 10)
+	{
+		count += print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+	return count + 1;
+}
+
+int print_unsigned(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+	{
+		count += print_unsigned(n / 10);
+	}
+	putchar (n % 10 + '0');
+	return count + 1;
+}
+
+int print_octal(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 8)
+	{
+		count += print_octal(n / 8);
+	}
+	putchar(n % 8 + '0');
+	return count + 1;
+}
+
+int print_hex(unsigned int n, int uppercase)
+{
+	int count = 0;
+	int digit = n % 16;
+
+	if (n / 16)
+	{
+		count += print_hex( n / 16, uppercase);
+	}
+
+	if (digit < 10)
+	{
+		putchar(digit + '0');
+	}
+	else
+	{
+		putchar(digit - 10 + (uppercase ? 'A' : 'a'));
+	}
+	return + 1;
+}
+
+int print_binary(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 2)
+	{
+		count += print_binary(n / 2);
+	}
+	putchar(n % 2 + '0');
+	return count + 1;
+}
+
 /**
 *_printf - count characters in string
 *@format: character string composed of zero or more directives
@@ -61,8 +137,8 @@ int _printf(const char *format, ...)
 			switch (*p)
 			{
 				case '%':
-				count += print_percent();
-				break;
+					count += print_percent();
+					break;
 				case 'c':
 					count += print_char(va_arg(args, int));
 					break;
@@ -72,9 +148,29 @@ int _printf(const char *format, ...)
 				case 'r':
 					count += print_string("%r");
 					break;
+				case 'd':
+				case 'i':
+					count += print_number(va_arg(args, int));
+					break;
+				case 'u':
+					count += print_unsigned(va_arg(args, unsigned int));
+					break;
+				case 'o':
+					count += print_octal(va_arg(args, unsigned int));
+					break;
+				case 'x':
+					count += print_hex(va_arg(args, unsigned int), 0);
+					break;
+				case 'X':
+					count += print_hex(va_arg(args, unsigned int), 1);
+					break;
+				case 'b':
+					count += print_binary(va_arg(args, unsigned int));
+					break;
 				default:
+					putchar('%');
 					putchar(*p);
-					count++;
+					count += 2;
 					break;
 			}
 
