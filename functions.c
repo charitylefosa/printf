b8873144556985f97dafb6f3973237199526adca
Mcminn <mcminntheonly@gmail.com>
2023-04-25 19:28:41 -0700

    update

--- functions.c
+++ functions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 
 /**
 *print_percent - handles percent conversion specifiers
@@ -130,68 +131,4 @@ int print_binary(unsigned int n)
 	return (count + 1);
 }
 
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int count;
-	const char *p;
 
-	va_start(args, format);
-
-	p = format;
-	count = 0;
-	while (*p)	{
-		if (*p == '%')
-		{
-			p++;
-			switch (*p)
-			{
-				case '%':
-					count += print_percent();
-					break;
-				case 'c':
-					count += print_char(va_arg(args, int));
-					break;
-				case 's':
-					count += print_string(va_arg(args, const char *));
-					break;
-				case 'r':
-					count += print_string("%r");
-					break;
-				case 'd':
-				case 'i':
-					count += print_number(va_arg(args, int));
-					break;
-				case 'u':
-					count += print_unsigned(va_arg(args, unsigned int));
-					break;
-				case 'o':
-					count += print_octal(va_arg(args, unsigned int));
-					break;
-				case 'x':
-					count += print_hex(va_arg(args, unsigned int), 0);
-					break;
-				case 'X':
-					count += print_hex(va_arg(args, unsigned int), 1);
-					break;
-				case 'b':
-					count += print_binary(va_arg(args, unsigned int));
-					break;
-				default:
-					putchar('%');
-					putchar(*p);
-					count += 2;
-					break;
-			}
-
-		}
-		else
-		{
-			putchar(*p);
-			count++;
-		}
-		p++;
-	}
-	va_end(args);
-	return (count);
-}
--- main.h
+++ main.h
@@ -17,14 +17,25 @@ typedef struct printhandler
 	int (*f)(va_list ap, flags_t *f);
 } ph;
 
+
 int (*get_print(char s))(va_list, flags_t *);
 int get_flag(char s, flags_t *f);
 int _printf(const char *format, ...);
 int _putchar(char c);
+/**
 int print_percent(va_list l, flags_t *f);
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
+*/
 
+int print_percent(void);
+int print_char(char c);
+int print_string(const char *s);
+int print_number(int n);
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
+int print_binary(unsigned int n);
 
 
 
